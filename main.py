fe96658a4d4ecc1c6fb29852598dbdc1b843de8d
Dharanidharani12345 <144319273+Dharanidharani12345@users.noreply.github.com>
2023-09-20 11:52:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-def fact(n):
-	if n==0 or n==1:
-		return 1
-	else:
-		return n*fact(n-1)
-num =int(input("Enter the number:"))
-result=fact(num)
-print("The factorial value {} is {}".format(num,result))
+class BankAccount:
+  def __init__(self, account_number, account_holder_name, initial_balance = 0.0):
+    self.__account_number = account_number 
+    self.__account_holder_name = account_holder_name 
+    self.__account_balance = initial_balance 
+  def deposit(self, amount): 
+    if amount >0:
+      self.__account_balance += amount 
+      print("Deposited ₹{}.New balance: ₹{}".format(amount,                    self.__account_balance)) 
+    else:
+      print("Invalid deposit amount")
+  def withdraw(self, amount):
+    if amount >0 and amount <= self.__account_balance:
+      print("Withdrew ₹{}.New balance:₹{}".format(amount, self.__account_balance))
+    else:
+      print("Invalid withdrawal amount or insufficient balance")
+  def display_balance(self):
+    print("Account balance for {}(account #{}):₹{}".format(self.__account_holder_name, self.__account_number, self.__account_balance))
+account = BankAccount(account_number = "123456789", account_holder_name = "chandru", initial_balance = 5000.0)
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.display_balance()
